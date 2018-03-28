7da9e8cf1a1ab5b2a4f46c0f9aab310c8ebeef31
MJY <Ma@163.com>
2018-03-28 20:16:31 +0800

    ihome  urlib

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ import redis
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1/flask_01'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = '/oxz/7k162x6X5bHoGIaguhFJG3zvDkg'
+    # SECRET_KEY = '/oxz/7k162x6X5bHoGIaguhFJG3zvDkg'
+    SECRET_KEY = 'base64.b64encode(os.urandom(24))'
 
 class ProductionConfig(Config):
     pass
--- ihome/__init__.py
+++ ihome/__init__.py
@@ -2,17 +2,30 @@
 '''app db 蓝图日志的生成'''
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from ihome.api_1_0 import api  # 注册蓝图用
+# from ihome.api_1_0 import api  # 注册蓝图用
+from flask_wtf import CSRFProtect
+
+db = SQLAlchemy()
+
+
 # 参数config_class接收配置文件的开发模式类 Developmentconfig Productionconfig
 def create_appdb(config_class):
     # 创建应用
     # 创建Flask类的实例， 即WIGS应用程序
     app = Flask(__name__)
     # 创建数据库
-    db = SQLAlchemy(app)
+
+    # db = SQLAlchemy(app)
     #调用数据库的配置
     app.config.from_object(config_class)
+    # 惰性加载app
+    db.init_app(app)
 
+    # CSRF保护 针对post/put/delete等会修改数据的请求，需要开启保护
+    CSRFProtect(app)
+
+    # 蓝图的导入, 可以用到时在加载, 以避免循环导入的问题
     # 注册蓝图
+    from ihome.api_1_0 import api  # 注册蓝图用
     app.register_blueprint(api)
     return app,db
